96259bdcd7627a9c4eed05f10dce7aa0a96da5e9
Allam Chaves Zamora <allam@una.cr>
2023-04-13 16:46:24 -0600

    ajustes

--- app/src/main/java/cr/ac/menufragment/MainActivity.kt
+++ app/src/main/java/cr/ac/menufragment/MainActivity.kt
@@ -13,20 +13,29 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-    DrawerLayout.DrawerListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    lateinit var  drawerLayout: DrawerLayout
+
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         var toolbar   = findViewById<Toolbar>(R.id.toolbar)
+
+
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         drawerLayout = findViewById(R.id.drawer_layout)
 
-        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        var toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -34,6 +43,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val navigationView = findViewById<NavigationView>(R.id.navigation_view)
         navigationView.setNavigationItemSelectedListener(this)
     }
+
 
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -43,17 +53,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override  fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
         val title: Int
+        lateinit var fragment: Fragment
         when (menuItem.getItemId()) {
-            R.id.nav_camera -> title = R.string.menu_camera
-            R.id.nav_gallery -> title = R.string.menu_gallery
+            R.id.nav_camera -> {
+                title = R.string.menu_camera
+                fragment = HomeFragment.newInstance(getString(title))
+            }
+            R.id.nav_gallery -> {
+                title = R.string.menu_gallery
+                fragment= MapsFragment()
+
+            }
             R.id.nav_manage -> title = R.string.menu_tools
-            R.id.nav_share -> title = R.string.menu_share
-            R.id.nav_send -> title = R.string.menu_send
+
             else -> throw IllegalArgumentException("menu option not implemented!!")
         }
-        val fragment: Fragment = HomeFragment.newInstance(getString(title))
+
         supportFragmentManager
             .beginTransaction()
             //.setCustomAnimations(R.anim.bottom_nav_enter, R.anim.bottom_nav_exit)
@@ -62,21 +79,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setTitle(getString(title))
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
-    }
-
-    override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onDrawerOpened(drawerView: View) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onDrawerClosed(drawerView: View) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onDrawerStateChanged(newState: Int) {
-        TODO("Not yet implemented")
     }
 }
